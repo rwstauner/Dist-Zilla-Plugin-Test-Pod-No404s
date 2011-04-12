566592a63cb30d0fa3957e0f526291469a15c2bb
Randy Stauner <randy@magnificent-tears.com>
2011-04-11 17:11:30 -0700

    Add code and DATA section so the module works

--- lib/Dist/Zilla/Plugin/Test/Pod/No404s.pm
+++ lib/Dist/Zilla/Plugin/Test/Pod/No404s.pm
@@ -4,6 +4,11 @@ package Dist::Zilla::Plugin::Test::Pod::No404s;
 
 use strict;
 use warnings;
+use Moose;
+extends 'Dist::Zilla::Plugin::InlineFiles';
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
 
 1;
 
@@ -12,3 +17,27 @@ use warnings;
 =head1 DESCRIPTION
 
 =cut
+
+__DATA__
+___[ xt/release/pod-no404s.t ]___
+#!perl
+
+use strict;
+use warnings;
+use Test::More;
+
+foreach my $env_skip ( qw(
+  SKIP_POD_NO404S
+  AUTOMATED_TESTING
+) ){
+  plan skip_all => "\$ENV{$env_skip} is set, skipping"
+    if $ENV{$env_skip};
+}
+
+eval "use Test::Pod::No404s";
+if ( $@ ) {
+  plan skip_all => 'Test::Pod::No404s required for testing POD';
+}
+else {
+  all_pod_files_ok();
+}
